93df0472fa3fae8b67da3bbd7630fdd07d2083ed
joamag <joamag@gmail.com>
2016-02-07 12:42:26 +0000

    initial commit

---
+++ src/mailme/base.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Mailme API
+# Copyright (c) 2008-2016 Hive Solutions Lda.
+#
+# This file is part of Hive Mailme API.
+#
+# Hive Mailme API is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hive Mailme API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Hive Mailme API. If not, see <http://www.gnu.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "GNU General Public License (GPL), Version 3"
+""" The license for the module """
+
+import appier
+
+from . import user
+
+BASE_URL = "https://mailme.bemisc.com/"
+""" The default base url to be used when no other
+base url value is provided to the constructor """
+
+class Api(
+    appier.Api,
+    user.UserApi
+):
+    """
+    Implementation of the Mailme API specification
+    for a simplified python client usage.
+    """
+
+    def __init__(self, *args, **kwargs):
+        appier.Api.__init__(self, *args, **kwargs)
+        self.base_url = appier.conf("BASE_URL", BASE_URL)
+        self.base_url = kwargs.get("base_url", BASE_URL)
+
+    def send(self, payload):
+        url = self.base_url + "send"
+        contents = self.post(url, data_j = payload)
+        return contents
